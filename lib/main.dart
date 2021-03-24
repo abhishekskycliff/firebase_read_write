f0677d2d2f1e74de385f733fd79fc0c288b5a067
abhishek bandi <abandi@skycliffit.com>
2021-03-24 09:55:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_new_app_for_web/newscreen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+// This widget is the root
+// of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase',
+      home: AddData(),
+    );
+  }
+}
+
+class AddData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("My App"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Upload data"),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => NewScreen()));
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('data')
+              .add({'text': 'https://i.stack.imgur.com/gqKFh.png'});
+        },
+      ),
+    );
+  }
+}
